6bc7766ebcab3abdbcac84aaa3c072c421caa2c4
Roberto D <roberto.dopazo15@gmail.com>
2024-02-26 11:30:30 -0400

    [ADD] .htaccess for httpd or apache in root folder and public folder also it was added BasePath package to get current path

--- public/index.php
+++ public/index.php
@@ -1,16 +1,26 @@
 <?php
+use Error;
 use App\Routes\WebRoutes;
 use Slim\Factory\AppFactory;
+use Selective\BasePath\BasePathMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Instantiate App
-$app = AppFactory::create();
+error_reporting(E_ALL);
 
-// Add error middleware
-$app->addErrorMiddleware(true, true, true);
-
-// Add routes
-WebRoutes::add($app);
-
-$app->run();
+try {
+  // Instantiate App
+  $app = AppFactory::create();
+  
+  //Check base path in server
+  $app->add(new BasePathMiddleware($app));
+  
+  // Add error middleware
+  $app->addErrorMiddleware(true, true, true);
+  // Add routes
+  WebRoutes::add($app);
+  
+  $app->run();
+} catch (Error $e) {
+  echo $e->getMessage();
+}
